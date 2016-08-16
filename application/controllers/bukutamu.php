1016b9f5ec96640b23834df4c76cb60319a327fc
Wahyu Kodar <wahyu.kodar@indosystem.com>
2016-08-16 11:16:40 +0700

    index aja

--- application/controllers/bukutamu.php
+++
@@ -1,8 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-class BukuTamu extends CI_Controller {
-	public function index()
-	{
-		echo ""; die();
-	}
-}
--- application/models/mymodel.php
+++
@@ -1,8 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-class Mymodel extends CI_Model {
-public function GetMahasiswa(){
-	$data=$this->db->query('select * from mahasiswa');
-	return $data->result_array();
-}
-}
